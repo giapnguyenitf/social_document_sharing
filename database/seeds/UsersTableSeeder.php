d59dd560cf67553db5a0f29c964e4a43ea91a5c0
giapnguyenitf <giapnguyenitf@gmail.com>
2018-05-31 11:32:54 +0700

    fix-deploy-seed

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -16,7 +16,7 @@ class UsersTableSeeder extends Seeder
         User::create([
             'name' => 'admin',
             'email' => 'admin@gmail.com',
-            'password' => 'notknow#1',
+            'password' => bcrypt('notknow#1'),
             'rules' => 2,
         ]);
 
